0df2099d40b0f51bb9db719a6cbf703d650fa1d9
Nabeel M.S <85822073+NabeelMS01@users.noreply.github.com>
2022-07-21 12:04:22 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Banner from './Components/Banner/Banner';
+import NavBar from './Components/NavBar/NavBar';
+import RowPost from './Components/RowPost/RowPost';
+import {actions,originals} from './urls.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar/>
+      <Banner/>
+      <RowPost title='Netflix Originals' urls={originals} />
+       <RowPost title='Action Movies' urls={actions} isSmall />
     </div>
   );
 }
